8f4cb4751a2913f046997c960d62445563b6ea35
tassouma228 <assouma@et.esiea.fr>
2020-12-30 11:06:37 +0100

    RecylerView 1.2

--- app/src/main/java/com/esiea/issofeapp/data/api/MovieDbClient.kt
+++ app/src/main/java/com/esiea/issofeapp/data/api/MovieDbClient.kt
@@ -14,6 +14,9 @@ const val BASE_URL = "https://api.themoviedb.org/3/"
 
 const val POSTER_BASE_URL = "https://image.tmdb.org/t/p/w342"
 
+const val FIRST_PAGE = 1
+const val POST_PER_PAGE = 20
+
 object MovieDbClient {
     fun getClient(): MovieDbInterface{
         val requestInterceptor = Interceptor{ chain ->
